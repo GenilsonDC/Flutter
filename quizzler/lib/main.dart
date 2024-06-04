9d4f6b638aef7948c9bf06c714616b56e472aee3
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-06-03 23:35:55 -0300

    TODO: Create a Question Class

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -30,7 +30,27 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int questionNumber = 0;
+
   List<Icon> scoreIcons = [];
+
+  List<String> questions = [
+    'O sol é uma estrela.',
+    'Os pinguins podem voar.',
+    'A Terra é o terceiro planeta do sistema solar.',
+    'As girafas têm o pescoço mais curto que os elefantes.',
+    'Os peixes respiram através das brânquias.',
+    'O maior mamífero do mundo é a baleia azul.',
+    'Todos os ursos polares vivem na Antártica.',
+    'O canguru é um animal que vive na Austrália.',
+    'As abelhas fazem mel.',
+    'O mês de fevereiro sempre tem 30 dias.',
+    'As bananas crescem em árvores.',
+    'O oceano Pacífico é o maior oceano do mundo.',
+    'As aranhas têm seis patas.',
+    'Os humanos têm cinco sentidos: visão, audição, olfato, paladar e tato.',
+    'As tartarugas podem viver por mais de 100 anos.',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -38,13 +58,13 @@ class _QuizPageState extends State<QuizPage> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions.first,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -77,7 +97,6 @@ class _QuizPageState extends State<QuizPage> {
                     ),
                   );
                 });
-
               },
             ),
           ),
@@ -105,7 +124,6 @@ class _QuizPageState extends State<QuizPage> {
                     ),
                   );
                 });
-
               },
             ),
           ),
@@ -119,7 +137,35 @@ class _QuizPageState extends State<QuizPage> {
 }
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
+
+1. **O sol é uma estrela.**   -true
+
+2. **Os pinguins podem voar.**   - False
+
+3. **A Terra é o terceiro planeta do sistema solar.**   - True
+
+4. **As girafas têm o pescoço mais curto que os elefantes.**   - False
+
+5. **Os peixes respiram através das brânquias.**   - True
+
+6. **O maior mamífero do mundo é a baleia azul.**   - True
+
+7. **Todos os ursos polares vivem na Antártica.**   - False
+
+8. **O canguru é um animal que vive na Austrália.**   - True
+
+9. **As abelhas fazem mel.**   - True
+
+10. **O mês de fevereiro sempre tem 30 dias.**    - False
+
+11. **As bananas crescem em árvores.**    - False (Elas crescem em plantas que parecem árvores, mas tecnicamente não são árvores.)
+
+12. **O oceano Pacífico é o maior oceano do mundo.**    - True
+
+13. **As aranhas têm seis patas.**    - False (Elas têm oito patas.)
+
+14. **Os humanos têm cinco sentidos: visão, audição, olfato, paladar e tato.**    - True
+
+15. **As tartarugas podem viver por mais de 100 anos.**    - True
+
 */
