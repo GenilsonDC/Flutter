c632b728fb548da096a00801a87c6868fa03a0af
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-06-20 10:33:56 -0300

    add Box decoration

--- bmi_calculator/lib/reusable_card.dart
+++ bmi_calculator/lib/reusable_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class ReusableCard extends StatelessWidget {
   const ReusableCard(
       {super.key, required this.customColor, required this.cardChild});
@@ -11,7 +12,17 @@ class ReusableCard extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.all(10.0),
       decoration: BoxDecoration(
-          color: customColor, borderRadius: BorderRadius.circular(10.0)),
+        color: customColor,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.5),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          )
+        ],
+      ),
       child: cardChild,
     );
   }
