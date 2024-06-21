9ed467bb24e5c2ca493f52236b000a4cfccffb0b
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-06-21 18:35:35 -0300

    Readme

--- bmi_calculator/README.md
+++ bmi_calculator/README.md
@@ -1,16 +1,13 @@
-# bmi_calculator
+# [![linkedin](Documentation/linkedin.png)](https://www.linkedin.com/in/genilson-do-carmo-8a42b89a/) [![instagram](Documentation/instagram.png)](https://www.instagram.com/genilson_carmo/)
 
-A new Flutter project.
+## Skills
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+| [<img src="Documentation/gif1.gif" alt="gif animado do primeiro app em Flutter e dart" />](https://github.com/GenilsonDC/Flutter/tree/main/mi_card) |
+| :----------------------------------------------------------- |
+
+   
+
--- bmi_calculator/lib/calculate_logic.dart
+++ bmi_calculator/lib/calculate_logic.dart
@@ -16,19 +16,19 @@ class CalculateLogic {
 
   String calculateBMI() {
     _bmi = weight / pow(height / 100, 2);
-    // Adjust BMI based on sex
+
     if (sex == 'male') {
-      _bmi *= 1.1; // Example adjustment for males
+      _bmi *= 1.1;
     } else if (sex == 'female') {
-      _bmi *= 0.9; // Example adjustment for females
+      _bmi *= 0.9;
     }
     return _bmi.toStringAsFixed(1);
   }
 
   String getResult() {
-    if (_bmi >= 25) {
+    if (_bmi >= 25.9) {
       return 'Overweight';
-    } else if (_bmi > 18.5) {
+    } else if (_bmi > 18) {
       return 'Normal';
     } else {
       return 'Underweight';
@@ -36,9 +36,9 @@ class CalculateLogic {
   }
 
   String getResultMessage() {
-    if (_bmi >= 25) {
+    if (_bmi >= 25.9) {
       return 'You have a higher than normal body weight. Try to exercise more and eat low-carb meals.';
-    } else if (_bmi > 18.5) {
+    } else if (_bmi > 18) {
       return 'You have a normal body weight. Congratulations. But always have checkups with your doctor.';
     } else {
       return 'You have a lower than normal body weight. Try to eat foods with more carbohydrates and proteins.';
